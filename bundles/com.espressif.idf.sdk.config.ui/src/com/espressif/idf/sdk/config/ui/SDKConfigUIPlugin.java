8da6ef66f86a8b0355ba6f531f0d4a969e0371ed
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2019-03-25 14:12:16 +0530

    Feat: Adding SDK Configuration editor
    Adding initial plugins
    
    https://jira.espressif.com:8443/browse/IEP-4

---
+++ bundles/com.espressif.idf.sdk.config.ui/src/com/espressif/idf/sdk/config/ui/SDKConfigUIPlugin.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright 2018-2019 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.sdk.config.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Kondal Kolipaka <kondal.kolipaka@espressif.com>
+ * 
+ * The activator class controls the plug-in life cycle
+ *
+ */
+public class SDKConfigUIPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.espressif.idf.sdk.config.ui"; //$NON-NLS-1$
+
+	// The shared instance
+	private static SDKConfigUIPlugin plugin;
+
+	/**
+	 * The constructor
+	 */
+	public SDKConfigUIPlugin() {
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static SDKConfigUIPlugin getDefault() {
+		return plugin;
+	}
+
+}
