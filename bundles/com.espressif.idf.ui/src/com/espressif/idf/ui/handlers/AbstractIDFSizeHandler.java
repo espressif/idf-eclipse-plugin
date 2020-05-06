f348c5705d47ff5d57104c19a634b24a41ea971c
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2020-05-06 15:57:38 +0530

    Code refactoring for idf size impl

---
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/AbstractIDFSizeHandler.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright 2020 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.ui.handlers;
+
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPageLayout;
+
+import com.espressif.idf.core.IDFCorePlugin;
+import com.espressif.idf.core.ProcessBuilderFactory;
+import com.espressif.idf.core.logging.Logger;
+import com.espressif.idf.core.util.IDFUtil;
+import com.espressif.idf.core.util.StringUtil;
+import com.espressif.idf.ui.IDFConsole;
+import com.espressif.idf.ui.UIPlugin;
+
+/**
+ * @author Kondal Kolipaka <kondal.kolipaka@espressif.com>
+ *
+ */
+public abstract class AbstractIDFSizeHandler extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		// get the selected project
+		IResource project = EclipseHandler.getSelectedProject(IPageLayout.ID_PROJECT_EXPLORER);
+		if (project == null)
+		{
+			project = EclipseHandler.getSelectedResource((IEvaluationContext) event.getApplicationContext());
+		}
+
+		if (project == null)
+		{
+			return project;
+		}
+
+		if (project instanceof IProject)
+		{
+			try
+			{
+				IStatus status = runIdfSizeCommand((IProject) project);
+				String message = status.getMessage();
+				Logger.log(message);
+				if (status.isOK() && !StringUtil.isEmpty(message))
+				{
+					IDFConsole idfConsole = new IDFConsole();
+					idfConsole.getConsoleStream().print(message);
+				}
+			}
+			catch (Exception e)
+			{
+				Logger.log(e);
+				MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", e.getMessage()); //$NON-NLS-1$
+			}
+		}
+
+		return null;
+	}
+
+	protected IStatus runIdfSizeCommand(IProject project) throws Exception
+	{
+		// Check IDF_PYTHON_ENV_PATH
+		String pythonExecutablenPath = IDFUtil.getIDFPythonEnvPath();
+		if (StringUtil.isEmpty(pythonExecutablenPath))
+		{
+			throw new Exception("IDF_PYTHON_ENV_PATH path is not found in the Eclispe CDT build environment variables"); //$NON-NLS-1$
+		}
+
+		// Check /project/build/projectname.map
+		IPath mapPath = project.getLocation().append("build").append(project.getName() + ".map"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (!mapPath.toFile().exists())
+		{
+			String msg = NLS.bind("Couldn't find {0} map file in the project", mapPath.toOSString()); //$NON-NLS-1$
+			throw new Exception(msg);
+		}
+
+		List<String> arguments = getCommandArgs(pythonExecutablenPath, mapPath);
+
+		ProcessBuilderFactory processRunner = new ProcessBuilderFactory();
+		try
+		{
+			return processRunner.runInBackground(arguments, Path.ROOT, System.getenv());
+		}
+		catch (Exception e1)
+		{
+			Logger.log(IDFCorePlugin.getPlugin(), e1);
+			return new Status(Status.ERROR, UIPlugin.PLUGIN_ID, e1.getMessage());
+		}
+	}
+
+	protected abstract List<String> getCommandArgs(String pythonExecutablenPath, IPath mapPath);
+
+}
--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/IDFSizeArchivesMemoryHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/IDFSizeArchivesMemoryHandler.java
@@ -7,109 +7,27 @@ package com.espressif.idf.ui.handlers;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.expressions.IEvaluationContext;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.ui.IPageLayout;
 
-import com.espressif.idf.core.IDFCorePlugin;
-import com.espressif.idf.core.ProcessBuilderFactory;
-import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
-import com.espressif.idf.core.util.StringUtil;
-import com.espressif.idf.ui.IDFConsole;
 
 /**
  * @author Kondal Kolipaka <kondal.kolipaka@espressif.com>
  *
  */
-public class IDFSizeArchivesMemoryHandler extends AbstractHandler implements IHandler
+public class IDFSizeArchivesMemoryHandler extends AbstractIDFSizeHandler
 {
-
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException
+	protected List<String> getCommandArgs(String pythonExecutablenPath, IPath mapPath)
 	{
-		// get the selected project
-		IResource project = EclipseHandler.getSelectedProject(IPageLayout.ID_PROJECT_EXPLORER);
-		if (project == null)
-		{
-			project = EclipseHandler.getSelectedResource((IEvaluationContext) event.getApplicationContext());
-		}
-
-		if (project == null)
-		{
-			return project;
-		}
-
-		// get the active server instance for the project
-		if (project instanceof IProject)
-		{
-			try
-			{
-				IStatus status = runIdfSizeArchivesCommand((IProject) project);
-				Logger.log(status.getMessage());
-
-				if (status.isOK())
-				{
-					String message = status.getMessage();
-					if (!StringUtil.isEmpty(message))
-					{
-						IDFConsole idfConsole = new IDFConsole();
-						idfConsole.getConsoleStream().print(message);
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				Logger.log(e);
-			}
-		}
-
-		return null;
-	}
-
-	protected IStatus runIdfSizeArchivesCommand(IProject project) throws Exception
-	{
-		// Check IDF_PYTHON_ENV_PATH
-		String pythonExecutablenPath = IDFUtil.getIDFPythonEnvPath();
-		if (StringUtil.isEmpty(pythonExecutablenPath))
-		{
-			throw new Exception("IDF_PYTHON_ENV_PATH path is not found in the Eclispe CDT build environment variables");
-		}
-
-		// Check /project/build/projectname.map
-		IPath mapPath = project.getLocation().append("build").append(project.getName() + ".map");
-		if (!mapPath.toFile().exists())
-		{
-			String msg = NLS.bind("Couldn't find {0} map file in the project", mapPath.toOSString());
-			throw new Exception(msg);
-		}
-
 		List<String> arguments = new ArrayList<String>();
 		arguments.add(pythonExecutablenPath);
 		arguments.add(IDFUtil.getIDFSizeScriptFile().getAbsolutePath());
 		arguments.add(mapPath.toOSString());
-		arguments.add("--archives");
-		arguments.add("--json");
+		arguments.add("--archives"); //$NON-NLS-1$
+		arguments.add("--json");//$NON-NLS-1$
 
-		ProcessBuilderFactory processRunner = new ProcessBuilderFactory();
-		try
-		{
-			return processRunner.runInBackground(arguments, Path.ROOT, System.getenv());
-		}
-		catch (Exception e1)
-		{
-			Logger.log(IDFCorePlugin.getPlugin(), e1);
-		}
-		return null;
+		return arguments;
 	}
 
 }
