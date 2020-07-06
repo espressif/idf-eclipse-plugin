e3ccbe420524b56ec0e33ddb0e25793ec3281649
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2020-07-06 15:10:37 +0530

    Cleanup build directory when IDF_TARGET changes to the project

--- bundles/com.espressif.idf.launch.serial.ui/src/com/espressif/idf/launch/serial/ui/internal/NewSerialFlashTargetWizard.java
+++ bundles/com.espressif.idf.launch.serial.ui/src/com/espressif/idf/launch/serial/ui/internal/NewSerialFlashTargetWizard.java
@@ -15,14 +15,31 @@
  *******************************************************************************/
 package com.espressif.idf.launch.serial.ui.internal;
 
+import java.io.File;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.launchbar.core.ILaunchBarManager;
 import org.eclipse.launchbar.core.target.ILaunchTarget;
 import org.eclipse.launchbar.core.target.ILaunchTargetManager;
 import org.eclipse.launchbar.core.target.ILaunchTargetWorkingCopy;
 import org.eclipse.launchbar.ui.target.LaunchTargetWizard;
+import org.eclipse.swt.widgets.Display;
 import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
+import com.espressif.idf.core.IDFCorePlugin;
+import com.espressif.idf.core.logging.Logger;
+import com.espressif.idf.core.util.SDKConfigJsonReader;
 import com.espressif.idf.launch.serial.SerialFlashLaunchTargetProvider;
 
 public class NewSerialFlashTargetWizard extends LaunchTargetWizard {
@@ -61,8 +78,86 @@ public class NewSerialFlashTargetWizard extends LaunchTargetWizard {
 		wc.save();
 
 		storeLastUsedSerialPort();
+		String newIdfTarget = page.getIDFTarget();
+
+		Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				update(newIdfTarget);
+			}
+
+			private void update(String newTarget) {
+				//Get old IDF target for the project
+				try {
+					ILaunchBarManager launchBarManager = Activator.getService(ILaunchBarManager.class);
+					ILaunchConfiguration config = launchBarManager.getActiveLaunchConfiguration();
+					String projectName = config.getName();
+					if (projectName != null && projectName.length() > 0) {
+
+						//Get IProject
+						IWorkspace workspace = ResourcesPlugin.getWorkspace();
+						IResource project = workspace.getRoot().findMember(projectName);
+
+						Logger.log("Project Name:: " + projectName); //$NON-NLS-1$
+						Logger.log("Project:: " + project); //$NON-NLS-1$
+
+						//build folder exist?
+						if (project != null) {
+							File buildLocation = new File(project.getLocation() + "/build"); //$NON-NLS-1$
+							if (buildLocation.exists()) {
+								//get current target
+								String currentTarget = new SDKConfigJsonReader((IProject) project)
+										.getValue("IDF_TARGET"); //$NON-NLS-1$
+
+								//If both are not same
+								if (currentTarget != null && !newTarget.equals(currentTarget)) {
+									boolean isDelete = MessageDialog.openQuestion(Display.getDefault().getActiveShell(),
+											"IDF TARGET Changed", //$NON-NLS-1$
+											"IDF_TARGET has changed for the project. Do you want to delete the `build` folder for the project?"); //$NON-NLS-1$
+									if (isDelete) {
+										IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+
+											@Override
+											public void run(IProgressMonitor monitor) throws CoreException {
+
+												monitor.beginTask("Deleting build folder...", 1); //$NON-NLS-1$
+												Logger.log("Deleting build folder " + buildLocation.getAbsolutePath()); //$NON-NLS-1$
+												deleteDirectory(buildLocation);
+												project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+											}
+										};
+
+										//run workspace job
+										try {
+											ResourcesPlugin.getWorkspace().run(runnable, new NullProgressMonitor());
+										} catch (Exception e1) {
+											Logger.log(IDFCorePlugin.getPlugin(), "Unable to delete the build folder", //$NON-NLS-1$
+													e1);
+										}
+									}
+								}
+							}
+						}
+
+					}
+
+				} catch (CoreException e1) {
+					Logger.log(e1);
+				}
+			}
+		});
 
 		return true;
+	}
+
+	private boolean deleteDirectory(File directoryToBeDeleted) {
+		File[] allContents = directoryToBeDeleted.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				deleteDirectory(file);
+			}
+		}
+		return directoryToBeDeleted.delete();
 	}
 
 	private void storeLastUsedSerialPort() {
