d62eccb6a03b07967b3a4785340813319517df55
Denys Almazov <sigma@f0xz.org.ua>
2020-12-01 15:52:37 +0100

    IEP-187: Add project name to size analysis editor (IEP-291) (#160)
    Added project name to analysis size editor tab name

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/size/IDFSizeAnalysisEditor.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/size/IDFSizeAnalysisEditor.java
@@ -13,8 +13,12 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.ui.part.MultiPageSelectionProvider;
 
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.SDKConfigJsonReader;
@@ -28,18 +32,26 @@ public class IDFSizeAnalysisEditor extends MultiPageEditorPart
 
 	public static String EDITOR_ID = "com.espressif.idf.ui.editor.idfsize"; //$NON-NLS-1$
 	private IProject project;
+	private IFile file;
 
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException
+	{
+		super.init(site, input);
+		FileEditorInput editorInput = (FileEditorInput) getEditorInput();
+		file = editorInput.getFile();
+		project = file.getProject();
+		setPartName(project.getName());
+	}
+	
 	@Override
 	protected void createPages()
 	{
-		FileEditorInput editorInput = (FileEditorInput) getEditorInput();
-		IFile file = editorInput.getFile();
 		String osString = file.getLocation().toOSString();
-		project = file.getProject();
 		Logger.log("Editor input:" + osString); //$NON-NLS-1$
-
-		createOverviewPage(file);
-		createDetailsPage(file);
+		
+		createOverviewPage();
+		createDetailsPage();
 	}
 
 	@Override
@@ -62,10 +74,8 @@ public class IDFSizeAnalysisEditor extends MultiPageEditorPart
 
 	/**
 	 * Create Size Analysis Details Page
-	 * 
-	 * @param file
 	 */
-	private void createDetailsPage(IFile file)
+	private void createDetailsPage()
 	{
 
 		Composite parent = new Composite(getContainer(), SWT.NONE);
@@ -103,10 +113,8 @@ public class IDFSizeAnalysisEditor extends MultiPageEditorPart
 
 	/**
 	 * Creates Size Analysis Overview Page
-	 * 
-	 * @param file
 	 */
-	private void createOverviewPage(IFile file)
+	private void createOverviewPage()
 	{
 		Composite parent = new Composite(getContainer(), SWT.NONE);
 		parent.setLayout(new FillLayout());
@@ -121,5 +129,4 @@ public class IDFSizeAnalysisEditor extends MultiPageEditorPart
 	{
 		return new SDKConfigJsonReader(project).getValue("IDF_TARGET");
 	}
-
 }
