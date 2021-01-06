57b871d437e4b4beb263811dca26359c709ac2ef
unknown <sigma@f0xz.org.ua>
2021-01-06 15:16:33 +0100

    Improve showing idf project templates readme format in the new project wizard

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/templates/FormBrowser.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/templates/FormBrowser.java
@@ -5,22 +5,32 @@
 
 package com.espressif.idf.ui.templates;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.LocationEvent;
+import org.eclipse.swt.browser.LocationListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.ScrolledFormText;
 
 import com.espressif.idf.core.IDFCorePlugin;
 
 public class FormBrowser {
 	FormToolkit toolkit;
 	Composite container;
-	ScrolledFormText formText;
+	Browser formText;
 	String text;
 	int style;
 
@@ -36,15 +46,42 @@ public class FormBrowser {
 		flayout.marginWidth = 1;
 		flayout.marginHeight = 1;
 		container.setLayout(flayout);
-		formText = new ScrolledFormText(container, SWT.V_SCROLL | SWT.H_SCROLL, false);
+		formText = new Browser(container, SWT.NONE);
+		LocationListener locationListener = new LocationListener()
+		{
+
+			@Override
+			public void changing(LocationEvent event)
+			{
+				try
+					{
+						PlatformUI.getWorkbench().getBrowserSupport().getExternalBrowser()
+								.openURL(new URL(event.location));
+						event.doit = false;
+					}
+					catch (
+							PartInitException
+							| MalformedURLException e)
+					{
+						e.printStackTrace();
+					}
+				formText.setText(text);
+			}
+
+			@Override
+			public void changed(LocationEvent event)
+			{
+
+			}
+
+		};
+		formText.addLocationListener(locationListener);
+
 		if (borderStyle == SWT.NULL) {
 			formText.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TREE_BORDER);
 			toolkit.paintBordersFor(container);
 		}
 		FormText ftext = toolkit.createFormText(formText, false);
-		formText.setFormText(ftext);
-		formText.setExpandHorizontal(true);
-		formText.setExpandVertical(true);
 		formText.setBackground(toolkit.getColors().getBackground());
 		formText.setForeground(toolkit.getColors().getForeground());
 		ftext.marginWidth = 2;
@@ -64,17 +101,26 @@ public class FormBrowser {
 		return container;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	public void setText(String text)
+	{
+		this.text = convertMarkdownToHtml(text);
 		if (formText != null)
 			try
 			{
-				formText.setText(text);
+				formText.setText(this.text);
 			}
 			catch (Exception e)
 			{
 				Status status = new Status(IStatus.ERROR, IDFCorePlugin.PLUGIN_ID, "Error parsing the template description", e); //$NON-NLS-1$
 				IDFCorePlugin.getPlugin().getLog().log(status);
 			}
+	}
+
+	private String convertMarkdownToHtml(String markdownText)
+	{
+		Parser parser = Parser.builder().build();
+		Node document = parser.parse(markdownText);
+		HtmlRenderer renderer = HtmlRenderer.builder().build();
+		return renderer.render(document);
 	}
 }
