70515f4666bad175bca300d9a16dc2dc1915738a
Denys Almazov <sigma@f0xz.org.ua>
2021-03-04 07:07:16 +0100

    IEP-42 UE improvments in product log, added null checks (#211)
    * UE improvments in product log, added null checks
    
    * null check in getPython function
    
    * adding not found msg to product info log

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/help/ProductInformationHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/help/ProductInformationHandler.java
@@ -8,6 +8,7 @@ package com.espressif.idf.ui.help;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -20,6 +21,7 @@ import com.espressif.idf.core.IDFEnvironmentVariables;
 import com.espressif.idf.core.ProcessBuilderFactory;
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
+import com.espressif.idf.core.util.StringUtil;
 import com.espressif.idf.ui.update.ListInstalledToolsHandler;
 import com.espressif.idf.ui.update.Messages;
 
@@ -29,33 +31,62 @@ public class ProductInformationHandler extends ListInstalledToolsHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
-		super.execute(event);
-		console.println(IDFEnvironmentVariables.IDF_PATH + ": "
-				+ new IDFEnvironmentVariables().getEnvValue(IDFEnvironmentVariables.IDF_PATH)); // $NON-NLS-1$
-		console.println(IDFEnvironmentVariables.IDF_PYTHON_ENV_PATH + ": "
-				+ new IDFEnvironmentVariables().getEnvValue(IDFEnvironmentVariables.IDF_PYTHON_ENV_PATH)); // $NON-NLS-1$
-		console.println(IDFEnvironmentVariables.PATH + ": "
-				+ new IDFEnvironmentVariables().getEnvValue(IDFEnvironmentVariables.PATH)); // $NON-NLS-1$
+
+		if (!StringUtil.isEmpty(getPythonExecutablePath()) && !StringUtil.isEmpty(IDFUtil.getIDFPath()))
+		{
+			super.execute(event);
+		}
+		else
+		{
+			activateIDFConsoleView();
+		}
+		String[] IDFEnvVarsToShow = { IDFEnvironmentVariables.IDF_PATH, IDFEnvironmentVariables.IDF_PYTHON_ENV_PATH,
+				IDFEnvironmentVariables.PATH };
+		showIDFEnvVars(IDFEnvVarsToShow);
 		console.println();
 		console.println(Messages.OperatingSystemMsg + System.getProperty("os.name").toLowerCase()); //$NON-NLS-1$
-		console.println(Messages.JavaRuntimeVersionMsg + System.getProperty("java.runtime.version")); //$NON-NLS-1$
-		console.println(Messages.EclipseMsg + Platform.getBundle("org.eclipse.platform").getVersion().toString()); //$NON-NLS-1$
-		console.println(Messages.EclipseCDTMsg + Platform.getBundle("org.eclipse.cdt").getVersion().toString()); //$NON-NLS-1$
+		console.println(Messages.JavaRuntimeVersionMsg
+				+ (Optional.ofNullable(System.getProperty("java.runtime.version")).orElse(Messages.NotFoundMsg))); //$NON-NLS-1$
+		console.println(Messages.EclipseMsg + (Optional.ofNullable(Platform.getBundle("org.eclipse.platform"))
+				.map(o -> o.getVersion().toString()).orElse(Messages.NotFoundMsg))); // $NON-NLS-1$
+		console.println(Messages.EclipseMsg + (Optional.ofNullable(Platform.getBundle("org.eclipse.cdt"))
+				.map(o -> o.getVersion().toString()).orElse(Messages.NotFoundMsg))); // $NON-NLS-1$
 		showEspIdfVersion();
-		console.println(Messages.PythonIdfEnvMsg + getPythonExeVersion(IDFUtil.getIDFPythonEnvPath()));
-		console.println(Messages.PythonPathMsg + getPythonExeVersion("python")); //$NON-NLS-1$
+		console.println(Messages.PythonIdfEnvMsg
+				+ (Optional.ofNullable(getPythonExeVersion(IDFUtil.getIDFPythonEnvPath()))
+						.orElse(Messages.NotFoundMsg)));
+		console.println(Messages.PythonPathMsg
+				+ (Optional.ofNullable(getPythonExeVersion("python")).orElse(Messages.NotFoundMsg))); //$NON-NLS-1$
 
 		return null;
 	}
 
+	private void showIDFEnvVars(String[] IDFEnvVarsToShow)
+	{
+		for (String IDFEnvVar : IDFEnvVarsToShow)
+		{
+			String IDFEnvVarValue = new IDFEnvironmentVariables().getEnvValue(IDFEnvVar);
+			IDFEnvVarValue = IDFEnvVarValue.isEmpty() ? Messages.NotFoundMsg : IDFEnvVarValue;
+			console.println(IDFEnvVar + ": " + IDFEnvVarValue); // $NON-NLS-1$
+		}
+	}
+
 	private void showEspIdfVersion()
 	{
-		List<String> commands = new ArrayList<>();
-		commands.add(IDFUtil.getIDFPythonEnvPath());
-		commands.add(IDFUtil.getIDFPythonScriptFile().getAbsolutePath());
-		commands.add("--version"); //$NON-NLS-1$
-		Map<String, String> envMap = new IDFEnvironmentVariables().getEnvMap();
-		console.println(runCommand(commands, envMap));
+		if (IDFUtil.getIDFPath() != null && IDFUtil.getIDFPythonEnvPath() != null)
+		{
+			List<String> commands = new ArrayList<>();
+			commands.add(IDFUtil.getIDFPythonEnvPath());
+			commands.add(IDFUtil.getIDFPythonScriptFile().getAbsolutePath());
+			commands.add("--version"); //$NON-NLS-1$
+			Map<String, String> envMap = new IDFEnvironmentVariables().getEnvMap();
+			console.println(runCommand(commands, envMap));
+		}
+		else
+		{
+			console.println(Messages.MissingIdfPathMsg);
+		}
+
 	}
 
 	private String getPythonExeVersion(String pythonExePath)
@@ -63,7 +94,7 @@ public class ProductInformationHandler extends ListInstalledToolsHandler
 		List<String> commands = new ArrayList<>();
 		commands.add(pythonExePath);
 		commands.add("--version"); //$NON-NLS-1$
-		return runCommand(commands, System.getenv());
+		return pythonExePath != null ? runCommand(commands, System.getenv()) : null;
 	}
 
 	private String runCommand(List<String> arguments, Map<String, String> env)
