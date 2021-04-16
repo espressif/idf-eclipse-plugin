5c4bcc42c978d420e6aaa0a4a9a968e3118d3e7f
Denys Almazov <sigma@f0xz.org.ua>
2021-04-16 14:44:49 +0200

    IEP-403 Fixing NPE in the new component wizard (#249)
    * Fixing NPE in the new component wizard
    
    * update selectedProject value with value from combo button
    
    * add newComponentWizard to the new shortcut
    
    * Update NewComponentWizardPage.java
    
    Co-authored-by: Kondal Kolipaka <kondal.kolipaka@espressif.com>

---
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/NewComponentHandler.java
@@ -0,0 +1,27 @@
+package com.espressif.idf.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+
+import com.espressif.idf.ui.wizard.NewComponentWizard;
+
+public class NewComponentHandler extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		NewComponentWizard newComponentWizard = new NewComponentWizard();
+		newComponentWizard.init(PlatformUI.getWorkbench(), null);
+
+		WizardDialog dialog = new WizardDialog(Display.getDefault().getActiveShell(), newComponentWizard);
+		dialog.create();
+		dialog.open();
+		return null;
+	}
+
+}
