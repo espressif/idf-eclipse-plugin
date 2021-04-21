57ab0192582702c4959ebf42c08d2950083dfece
Denys Almazov <sigma@f0xz.org.ua>
2021-04-21 11:27:29 +0200

    IEP-411 add a default value for project  (#253)
    * IEP-411_add_a_default_value_for_project
    
    * change approach in the default value selection

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/wizard/NewComponentWizardPage.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/wizard/NewComponentWizardPage.java
@@ -112,12 +112,13 @@ public class NewComponentWizardPage extends WizardPage
 		projectNameLbl.setText(Messages.NewComponentWizardPage_ProjectNameLbl);
 		projectCombo = new Combo(container, SWT.BORDER | SWT.READ_ONLY);
 		Optional<IProject> optProject = Optional.ofNullable(EclipseUtil.getSelectedProjectInExplorer());
-		optProject.ifPresent(project -> projectCombo.setText(project.getName()));
 		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
 		for (IProject project : projects)
 		{
 			projectCombo.add(project.getName());
+
 		}
+		optProject.ifPresent(project -> projectCombo.setText(project.getName()));
 		projectCombo.addModifyListener(new ModifyListener()
 		{
 			@Override
