52d3844e7b6e62853ba13a15700c504f90599312
Denys Almazov <sigma@f0xz.org.ua>
2021-06-23 16:20:25 +0200

    ÃÐadded menu Espressif with empty EraseFlashCommand

---
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/EraseFlashCommandHandler.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright 2021 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class EraseFlashCommandHandler extends AbstractHandler
+{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
