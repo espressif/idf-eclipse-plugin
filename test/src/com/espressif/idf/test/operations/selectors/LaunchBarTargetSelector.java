4bbbfb4d5f1a825cc591428cfccb8b24ab90a59a
alirana01 <85216275+alirana01@users.noreply.github.com>
2021-07-06 07:10:16 +0500

    IEP 445: SWTBot Automation (#300)
    * IEP-445: Initial test with the swtbot just to verify that existing tests project can fit this
    
    * IEP-457: Script for the missing properties
    
    The script is to be run on powershell in the root directory for the idf-eclipse-plugin, it will look for properties files in all directories against the given language locale and generate the output in missing properties folder keeping the same structure
    
    * IEP-445: Initial changes related to FT framework idea
    
    Simple test testing the creation of the new project is added.
    We still have to figure out a way to launch a proper eclipse env that is already setup with the required env variables and settings from esp-idf
    
    * IEP-445: Refining utils
    
    * IEP-445: Build verification added
    
    Builds waits can now be achieved now trying to match the file content
    
    * IEP-445: File Contents can now be verified
    
    * IEP-445: Test case updated
    
    * IEP-445: Package refactored
    
    * IEP-445: Cleaning the project structure
    
    * IEP-445: refactoring and organizing
    
    * IEP-445: formatting and java docs and minor refactoring
    
    * IEP-445: matching the branch with master changes
    
    * IEP-445: Env side test added
    
    * IEP-445: Env setup before running the tests
    
    * IEP-445: Review comments
    
    Updated the classpaths to reference workspace instead of local directories for javadocs and sources of attached libraries.
    Also updated minor code to make compatible with java1.8
    
    * IEP-445: Refactored the project name from tests to test
    
    * IEP-445: refactoring project name
    
    * IEP-445: review comments and code refactoring
    
    * IEP-445: New tests for setting env added
    
    * IEP-445: Sample tests launchbar for test
    
    * IEP-445: Initial test for verifying launch descriptor completed
    
    * IEP-445: Config Selector updated
    
    * IEP-445: Resolving invalid thread exceptions with UI
    
    New test cases and thread exceptions were handled and new test case to verify the launch configuration was also added
    
    * IEP-455: Some refactoring and launch target tests added
    
    * IEP-445: Initial draft of test for the plugin installation with job api to wait for the background processes as dependency
    
    * IEP-445: Test case updates
    
    * IEP-445: Review comments for java version and removing unwanted tests

---
+++ test/src/com/espressif/idf/test/operations/selectors/LaunchBarTargetSelector.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright 2021 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+
+package com.espressif.idf.test.operations.selectors;
+
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.withText;
+
+import org.eclipse.launchbar.ui.controls.internal.CSelector;
+import org.eclipse.launchbar.ui.controls.internal.LaunchBarWidgetIds;
+import org.eclipse.launchbar.ui.controls.internal.TargetSelector;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.SWTBotWidget;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
+import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.results.Result;
+import org.eclipse.swtbot.swt.finder.widgets.AbstractSWTBotControl;
+
+/**
+ * Launchbar CDT helper class to select items from launch targets
+ * 
+ * @author Ali Azam Rana
+ *
+ */
+@SuppressWarnings("restriction")
+@SWTBotWidget(clasz = CSelector.class, preferredName = "cselector")
+public class LaunchBarTargetSelector extends AbstractSWTBotControl<CSelector>
+{
+
+	public LaunchBarTargetSelector(TargetSelector targetSelector) throws WidgetNotFoundException
+	{
+		super(targetSelector);
+	}
+
+	public LaunchBarTargetSelector(SWTBot bot)
+	{
+		this(bot.widget(WidgetMatcherFactory.withTooltip("Launch Target: OK")));
+	}
+
+	public SWTBot bot()
+	{
+		return new SWTBot(widget);
+	}
+
+	public void click(int x, int y)
+	{
+		notify(SWT.MouseEnter);
+		notify(SWT.MouseMove);
+		notify(SWT.Activate);
+		notify(SWT.FocusIn);
+		notify(SWT.MouseDown, createMouseEvent(x, y, 1, SWT.NONE, 1));
+		notify(SWT.MouseUp, createMouseEvent(x, y, 1, SWT.BUTTON1, 1));
+	}
+
+	public void clickEdit()
+	{
+		click();
+		bot().buttonWithId(LaunchBarWidgetIds.EDIT).click(); // $NON-NLS-1$
+	}
+
+	private void clickOnInternalWidget(int x, int y, Widget internalWidget)
+	{
+		notify(SWT.MouseDown, createMouseEvent(x, y, 1, SWT.NONE, 1), internalWidget);
+		notify(SWT.MouseUp, createMouseEvent(x, y, 1, SWT.BUTTON1, 1), internalWidget);
+	}
+
+	@Override
+	public LaunchBarTargetSelector click()
+	{
+		Point size = syncExec((Result<Point>) () -> widget.getSize());
+		click(size.x / 2, size.y / 2);
+		return this;
+	}
+
+	public LaunchBarTargetSelector select(String text)
+	{
+		click();
+		Label itemToSelect = bot().shellWithId(LaunchBarWidgetIds.POPUP).bot().widget(withText(text));
+		Point itemToSelectLocation = syncExec((Result<Point>) () -> itemToSelect.getLocation());
+		clickOnInternalWidget(itemToSelectLocation.x, itemToSelectLocation.y, itemToSelect);
+		return this;
+	}
+}
