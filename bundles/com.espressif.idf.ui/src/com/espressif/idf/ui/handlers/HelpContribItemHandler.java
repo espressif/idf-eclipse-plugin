cee2e83fc495c1864f7970f98905d66298ffb49a
alirana01 <ali.azamrana@espressif.com>
2021-11-18 14:44:47 +0500

    IEP-527: Removed awt and classes and updated the way browser is invoked

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/HelpContribItemHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/handlers/HelpContribItemHandler.java
@@ -4,12 +4,16 @@
  *******************************************************************************/
 package com.espressif.idf.ui.handlers;
 
-import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URL;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 
 import com.espressif.idf.core.logging.Logger;
 
@@ -25,12 +29,21 @@ public class HelpContribItemHandler extends AbstractHandler
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
+		IWorkbenchBrowserSupport support = PlatformUI.getWorkbench().getBrowserSupport();
 		String url = "https://www.esp32.com/viewforum.php?f=40"; //$NON-NLS-1$
 		try
 		{
-			Desktop.getDesktop().browse(new URL(url).toURI());
+			IWebBrowser browser = support.getExternalBrowser();
+			if (browser != null)
+			{
+				URL docsUrl = new URL(url);
+				Logger.log(docsUrl.getPath());
+				browser.openURL(docsUrl);
+			}
 		}
-		catch (Exception e)
+		catch (
+				PartInitException
+				| IOException e)
 		{
 			Logger.log(e);
 		}
