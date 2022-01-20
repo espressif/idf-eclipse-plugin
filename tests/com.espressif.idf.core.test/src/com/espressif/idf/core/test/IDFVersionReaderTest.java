5710307c19f3362c180f6aef249b5fe6e337ccca
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2022-01-20 14:29:28 +0530

    test updates

---
+++ tests/com.espressif.idf.core.test/src/com/espressif/idf/core/test/IDFVersionReaderTest.java
@@ -0,0 +1,16 @@
+package com.espressif.idf.core.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IDFVersionReaderTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
