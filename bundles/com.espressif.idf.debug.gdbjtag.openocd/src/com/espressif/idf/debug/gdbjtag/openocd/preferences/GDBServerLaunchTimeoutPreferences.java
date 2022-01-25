cd5d50e2b97ce910de1040a1d35043237ef6d52d
alirana01 <ali.azamrana@espressif.com>
2022-01-25 12:29:08 +0500

    IEP-552
    - Added the settings page for the configuration
    - removed the hardcoded default
    Still need to figure out the way to properly read defaults from preferences store.

---
+++ bundles/com.espressif.idf.debug.gdbjtag.openocd/src/com/espressif/idf/debug/gdbjtag/openocd/preferences/GDBServerLaunchTimeoutPreferences.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright 2022 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.debug.gdbjtag.openocd.preferences;
+
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.espressif.idf.debug.gdbjtag.openocd.Activator;
+
+import ilg.gnumcueclipse.core.preferences.ScopedPreferenceStoreWithoutDefaults;
+
+/**
+ * Preferences page for setting the launch timeout
+ * @author Ali Azam Rana
+ *
+ */
+public class GDBServerLaunchTimeoutPreferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage
+{
+	public static final String ID = "com.espressif.idf.serial.monitor.monitorPreferencesPage"; //$NON-NLS-1$
+
+	public GDBServerLaunchTimeoutPreferences()
+	{
+		super(GRID);
+		setPreferenceStore(new ScopedPreferenceStoreWithoutDefaults(InstanceScope.INSTANCE, Activator.PLUGIN_ID));
+		setDescription(Messages.GDBServerTimeoutPage_Description);
+	}
+
+	@Override
+	protected void createFieldEditors()
+	{
+		FieldEditor fGdbServerLaunchTimeout = new IntegerFieldEditor(Activator.GDB_SERVER_LAUNCH_TIMEOUT,
+				Messages.GDBServerTimeoutPage_TimeoutField, getFieldEditorParent());
+		fGdbServerLaunchTimeout.setPreferenceStore(getPreferenceStore());
+		fGdbServerLaunchTimeout.load();
+		addField(fGdbServerLaunchTimeout);
+	}
+
+	@Override
+	public void init(IWorkbench workbench)
+	{
+	}
+}
