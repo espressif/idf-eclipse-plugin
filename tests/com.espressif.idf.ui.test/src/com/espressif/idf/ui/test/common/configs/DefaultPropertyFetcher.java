b599ca83c1e0c5a17bbdddccd6968e70e1638e7a
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2022-02-04 00:00:59 +0530

    IEP-625: Tests CI Integration and restructuring (#449)
    * test project structure
    
    /tests/idf.core.test
    /tests/idf.core.ui.test
    
    * Fix build

---
+++ tests/com.espressif.idf.ui.test/src/com/espressif/idf/ui/test/common/configs/DefaultPropertyFetcher.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright 2021 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.ui.test.common.configs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * The class to fetch the properties from the default config file specified in {@link IDefaultConfigConstants}
+ * 
+ * @author Ali Azam Rana
+ *
+ */
+public class DefaultPropertyFetcher
+{
+	/**
+	 * Gets the long property value from the default config file
+	 * ({@link IDefaultConfigConstants#DEFAULT_CONFIG_PROPERTY_FILE}) and if not found returns the default value
+	 * 
+	 * @param propertyName property name to look for
+	 * @param defaultValue the default value to return if property not present in file
+	 * @return the long value of the property
+	 * @throws IOException if the property file is not found
+	 */
+	public static long getLongPropertyValue(String propertyName, long defaultValue) throws IOException
+	{
+		Properties properties = new Properties();
+		InputStream inputStream = DefaultPropertyFetcher.class.getClassLoader()
+				.getResourceAsStream(IDefaultConfigConstants.DEFAULT_CONFIG_PROPERTY_FILE);
+
+		if (inputStream == null)
+		{
+			return defaultValue;
+		}
+
+		properties.load(inputStream);
+		if (properties.containsKey(propertyName))
+		{
+			return Long.valueOf(properties.getProperty(propertyName));
+		}
+
+		return defaultValue;
+	}
+
+	/**
+	 * Gets the String property value from the default config file
+	 * ({@link IDefaultConfigConstants#DEFAULT_CONFIG_PROPERTY_FILE}) and if not found returns the default value
+	 * 
+	 * @param propertyName property name to look for
+	 * @param defaultValue the default value to return if property not present in file
+	 * @return the long value of the property
+	 * @throws IOException if the property file is not found
+	 */
+	public static String getStringPropertyValue(String propertyName, String defaultValue) throws IOException
+	{
+		Properties properties = new Properties();
+		InputStream inputStream = DefaultPropertyFetcher.class.getClassLoader()
+				.getResourceAsStream(IDefaultConfigConstants.DEFAULT_CONFIG_PROPERTY_FILE);
+
+		if (inputStream == null)
+		{
+			return defaultValue;
+		}
+
+		properties.load(inputStream);
+		if (properties.containsKey(propertyName))
+		{
+			return properties.getProperty(propertyName);
+		}
+
+		return defaultValue;
+	}
+}
