30226dd3e46d1b68fdf042ed18a350ec66600945
unknown <sigma@f0xz.org.ua>
2022-02-22 11:24:38 +0200

    changed preference store for serial monitor parameters

--- bundles/com.espressif.idf.serial.monitor/src/com/espressif/idf/serial/monitor/core/LocalTerminal.java
+++ bundles/com.espressif.idf.serial.monitor/src/com/espressif/idf/serial/monitor/core/LocalTerminal.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.Platform;
+
 import com.espressif.idf.serial.monitor.SerialMonitorBundle;
+import com.espressif.idf.ui.UIPlugin;
 import com.pty4j.PtyProcess;
 import com.pty4j.PtyProcessBuilder;
 
@@ -19,7 +22,10 @@ public class LocalTerminal
 	private List<String> arguments;
 	private File workingDir;
 	private Map<String, String> environment;
-
+	
+	private final int DEFAULT_SERIAL_MONITOR_NUBMER_OF_LINES = 1000;
+	private final int DEFAULT_SERIAL_MONITOR_NUMBER_OF_CHARS_IN_LINE = 500;
+	
 	public LocalTerminal(List<String> commandArgs, File projectWorkingDir, Map<String, String> environment)
 	{
 		this.arguments = commandArgs;
@@ -30,10 +36,8 @@ public class LocalTerminal
 	public Process connect() throws IOException
 	{
 		String[] args = arguments.toArray(new String[arguments.size()]);
-		int numberOfRows = SerialMonitorBundle.getInstance().getPreferenceStore()
-				.getInt(SerialMonitorBundle.SERIAL_MONITOR_NUMBER_OF_LINES);
-		int numberOfCols = SerialMonitorBundle.getInstance().getPreferenceStore()
-				.getInt(SerialMonitorBundle.SERIAL_MONITOR_NUMBER_OF_CHARS_IN_LINE);
+		int numberOfRows =  Platform.getPreferencesService().getInt(UIPlugin.PLUGIN_ID, SerialMonitorBundle.SERIAL_MONITOR_NUMBER_OF_LINES, DEFAULT_SERIAL_MONITOR_NUBMER_OF_LINES, null);
+		int numberOfCols =  Platform.getPreferencesService().getInt(UIPlugin.PLUGIN_ID, SerialMonitorBundle.SERIAL_MONITOR_NUMBER_OF_CHARS_IN_LINE, DEFAULT_SERIAL_MONITOR_NUMBER_OF_CHARS_IN_LINE, null);
 		PtyProcessBuilder ptyProcessBuilder = new PtyProcessBuilder(args).setEnvironment(environment)
 				.setDirectory(workingDir.getAbsolutePath()).setInitialColumns(numberOfCols).setInitialRows(numberOfRows)
 				.setConsole(false).setCygwin(false).setLogFile(null);
