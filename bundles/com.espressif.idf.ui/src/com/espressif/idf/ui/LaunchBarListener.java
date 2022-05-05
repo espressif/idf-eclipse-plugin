fbf214ec573772a218b764844ecced01cecf7f2d
sigmaaa <sigma@f0xz.org.ua>
2022-05-05 13:04:50 +0300

    launchbar listener support for custom build folder

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/LaunchBarListener.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/LaunchBarListener.java
@@ -26,6 +26,7 @@ import org.eclipse.swt.widgets.Display;
 
 import com.espressif.idf.core.IDFCorePlugin;
 import com.espressif.idf.core.logging.Logger;
+import com.espressif.idf.core.util.IDFUtil;
 import com.espressif.idf.core.util.SDKConfigJsonReader;
 import com.espressif.idf.core.util.StringUtil;
 
@@ -79,7 +80,7 @@ public class LaunchBarListener implements ILaunchBarListener
 				// build folder exist?
 				if (project != null)
 				{
-					File buildLocation = new File(project.getLocation() + "/build"); //$NON-NLS-1$
+					File buildLocation = new File(IDFUtil.getBuildDir((IProject) project)); //$NON-NLS-1$
 					if (buildLocation.exists())
 					{
 						// get current target
