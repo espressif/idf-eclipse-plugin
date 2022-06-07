5dda55348f5c7f1aa40cb986e403ac8a1df4fad2
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2022-06-07 15:25:06 +0200

    Fix: Launch Application size analysis editor with external .map file (#539)

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/size/IDFSizeMemoryHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/size/IDFSizeMemoryHandler.java
@@ -9,11 +9,15 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IPageLayout;
@@ -22,6 +26,7 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.FileEditorInput;
 
+import com.espressif.idf.core.IDFConstants;
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
 import com.espressif.idf.ui.handlers.EclipseHandler;
@@ -58,16 +63,37 @@ public class IDFSizeMemoryHandler extends AbstractHandler
 			return null;
 
 		}
-		launchEditor(mapFilePath);
+		try
+		{
+			launchEditor(mapFilePath, (IProject) project);
+		}
+		catch (CoreException e)
+		{
+			Logger.log(e);
+		}
 
 		return null;
 	}
 
-	private void launchEditor(IPath mapFilePath)
+	private void launchEditor(IPath mapFilePath, IProject project) throws CoreException
 	{
-
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IFile iFile = workspace.getRoot().getFileForLocation(mapFilePath);
+		if (mapFilePath.toFile().exists() && iFile == null) // file is located outside of the workspace
+		{
+			// create a link in the project/build/ folder to open file in eclipse editor
+			IFolder buildRootFolder = project.getFolder(IDFConstants.BUILD_FOLDER);
+			iFile = buildRootFolder.getFile(mapFilePath.lastSegment());
+			if (!iFile.exists())
+			{
+				IProgressMonitor monitor = new NullProgressMonitor();
+				if (!buildRootFolder.exists())
+				{
+					buildRootFolder.create(IResource.FORCE | IResource.DERIVED, true, monitor);
+				}
+				iFile.createLink(mapFilePath, IResource.NONE, null);
+			}
+		}
 		FileEditorInput editorInput = new FileEditorInput(iFile);
 		Display.getDefault().asyncExec(new Runnable()
 		{
