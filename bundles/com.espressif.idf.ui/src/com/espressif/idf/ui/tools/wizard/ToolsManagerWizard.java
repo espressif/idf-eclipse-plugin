0d7389cc4f9a7db79fe2346a11a5aab9a0051804
alirana01 <ali.azamrana@espressif.com>
2022-06-20 10:42:41 +0200

    cleanup for unused imports

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/tools/wizard/ToolsManagerWizard.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/tools/wizard/ToolsManagerWizard.java
@@ -6,7 +6,6 @@ package com.espressif.idf.ui.tools.wizard;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
