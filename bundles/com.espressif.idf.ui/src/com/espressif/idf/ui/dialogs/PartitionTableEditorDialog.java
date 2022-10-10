9e1fdcfefd4d9a184ab8687202f8dac2831e217c
Denys Almazov <sigma@f0xz.org.ua>
2022-10-10 15:30:04 +0300

    WIP

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/dialogs/PartitionTableEditorDialog.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/dialogs/PartitionTableEditorDialog.java
@@ -6,12 +6,11 @@ import org.commonmark.node.Text;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 
 public class PartitionTableEditorDialog extends TitleAreaDialog
 {
@@ -37,11 +36,27 @@ public class PartitionTableEditorDialog extends TitleAreaDialog
 	@Override
 	protected Control createDialogArea(Composite parent)
 	{
-		Composite area = (Composite) super.createDialogArea(parent);
-		Composite container = new Composite(area, SWT.NONE);
-		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		GridLayout layout = new GridLayout(4, false);
-		container.setLayout(layout);
+		csvTable = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
+
+		TableColumn nameColumn = new TableColumn(csvTable, SWT.NULL);
+		nameColumn.setText(columnNames[0]);
+
+		TableColumn typeColumn = new TableColumn(csvTable, SWT.NULL);
+		typeColumn.setText(columnNames[1]);
+
+		TableColumn subTypeColumn = new TableColumn(csvTable, SWT.NULL);
+		subTypeColumn.setText(columnNames[2]);
+
+		TableColumn offSetColumn = new TableColumn(csvTable, SWT.NULL);
+		offSetColumn.setText(columnNames[3]);
+
+		TableColumn sizeColumn = new TableColumn(csvTable, SWT.NULL);
+		sizeColumn.setText(columnNames[4]);
+
+		TableColumn encryptedColumn = new TableColumn(csvTable, SWT.NULL);
+		encryptedColumn.setText(columnNames[5]);
+
+		tableViewer = new TableViewer(csvTable);
 		return super.createDialogArea(parent);
 	}
 
