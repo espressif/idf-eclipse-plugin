2416d02286b3fea2022547f41aa06fb0387fcc45
Denys Almazov <sigma@f0xz.org.ua>
2023-01-30 11:50:35 +0200

    fix return null value if multiple pythonVersions

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/AbstractToolsHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/AbstractToolsHandler.java
@@ -7,6 +7,7 @@ package com.espressif.idf.ui.update;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -50,13 +51,13 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
 		activateIDFConsoleView();
-		
-		String commmand_id = commandId;;
+
+		String commmand_id = commandId;
 		if (event != null)
 		{
 			commmand_id = event.getCommand().getId();
 		}
-		
+
 		Logger.log("Command id:" + commmand_id); //$NON-NLS-1$
 
 		// Get IDF_PATH
@@ -90,7 +91,8 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 		if (StringUtil.isEmpty(pythonExecutablenPath) || StringUtil.isEmpty(gitExecutablePath)
 				|| StringUtil.isEmpty(idfPath))
 		{
-			errorConsoleStream.print("One or more paths are empty! Make sure you provide IDF_PATH, git and python executables"); //$NON-NLS-1$
+			errorConsoleStream
+					.print("One or more paths are empty! Make sure you provide IDF_PATH, git and python executables"); //$NON-NLS-1$
 			return null;
 		}
 
@@ -102,11 +104,12 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 
 		return null;
 	}
-	
+
 	protected void activateIDFConsoleView()
 	{
 		console = new IDFConsole().getConsoleStream(Messages.IDFToolsHandler_ToolsManagerConsole, null, false);
-		errorConsoleStream = new IDFConsole().getConsoleStream(Messages.IDFToolsHandler_ToolsManagerConsole, null, true);
+		errorConsoleStream = new IDFConsole().getConsoleStream(Messages.IDFToolsHandler_ToolsManagerConsole, null,
+				true);
 	}
 
 	protected String getPythonExecutablePath()
@@ -120,11 +123,10 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 			{
 				Logger.log("No Python installations found in the system."); //$NON-NLS-1$
 			}
-			if (pythonVersions.size() == 1)
-			{
-				Map.Entry<String, String> entry = pythonVersions.entrySet().iterator().next();
-				pythonExecutablenPath = entry.getValue();
-			}
+			pythonExecutablenPath = pythonVersions.entrySet().stream()
+					.filter(e -> Double.parseDouble(e.getKey()) >= 3.6).map(Entry::getValue).findAny()
+					.orElseGet(IDFUtil::getPythonExecutable);
+
 		}
 		else
 		{
@@ -167,11 +169,12 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 			}
 			if (status.getSeverity() == IStatus.ERROR)
 			{
-				errorConsoleStream.print(status.getException() != null ? status.getException().getMessage() : status.getMessage());
+				errorConsoleStream.print(
+						status.getException() != null ? status.getException().getMessage() : status.getMessage());
 			}
 			console.println(status.getMessage());
 			console.println();
-			
+
 			return IDFCorePlugin.okStatus(status.getMessage(), null);
 		}
 		catch (Exception e1)
@@ -180,7 +183,7 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 			return IDFCorePlugin.errorStatus(e1.getMessage(), e1);
 		}
 	}
-	
+
 	protected String runCommand(List<String> arguments, Path workDir, Map<String, String> env)
 	{
 		String exportCmdOp = ""; //$NON-NLS-1$
@@ -234,7 +237,7 @@ public abstract class AbstractToolsHandler extends AbstractHandler
 
 		return builder.toString().trim();
 	}
-	
+
 	public void setCommandId(String commandId)
 	{
 		this.commandId = commandId;
