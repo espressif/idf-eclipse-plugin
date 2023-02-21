6aa9f499b11a40d9c7b218ceee0c64d0170dd3e2
Kondal Kolipaka <kondal.kolipaka@gmail.com>
2023-02-21 16:54:09 +0530

    wokwi simulator integration

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/install/WokWiSimulatorHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/install/WokWiSimulatorHandler.java
@@ -1,6 +1,7 @@
 package com.espressif.idf.ui.install;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -12,9 +13,11 @@ import org.eclipse.cdt.core.CCorePlugin;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
+//import org.eclipse.core.resources.IProject;
+//import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 
 import com.espressif.idf.core.ExecutableFinder;
@@ -23,7 +26,12 @@ import com.espressif.idf.core.ProcessBuilderFactory;
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
 import com.espressif.idf.core.util.SDKConfigJsonReader;
-import com.espressif.idf.ui.handlers.EclipseHandler;
+import com.espressif.idf.core.util.StringUtil;
+import com.espressif.idf.ui.EclipseUtil;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
 
 /**
  * @author Kondal Kolipaka
@@ -32,119 +40,163 @@ import com.espressif.idf.ui.handlers.EclipseHandler;
 public class WokWiSimulatorHandler extends AbstractHandler
 {
 
+	private static final String WOKWI_CONFIG_JSON = "wokwi-config.json";//$NON-NLS-1$
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
 
-		IProject project = EclipseHandler.getSelectedResource((IEvaluationContext) event.getApplicationContext())
-				.getProject();
-
-		if (project != null)
+		IProject project = getProject();
+		if (project == null)
 		{
-			SDKConfigJsonReader sdkconfig = new SDKConfigJsonReader(project);
-			String target = sdkconfig.getValue("IDF_TARGET"); //$NON-NLS-1$
+			Logger.log("Project can't be null. Make sure project is selected in the project explorer.");
+			return null;
 
-			String elfFilePath = IDFUtil.getELFFilePath(project).toOSString();
+		}
+		SDKConfigJsonReader sdkconfig = new SDKConfigJsonReader(project);
+		String target = sdkconfig.getValue("IDF_TARGET"); //$NON-NLS-1$
 
-			// Look for wokwi path
-			String wokwiExecutablePath = "/Users/kondal/.cargo/bin/wokwi-server";
-			IPath wokwiPath = ExecutableFinder.find("wokwi-server", false); //$NON-NLS-1$
-			Logger.log("wokwiPath path:" + wokwiPath); //$NON-NLS-1$
-			if (wokwiExecutablePath == null)
-			{
-				List<String> arguments = new ArrayList<String>();
-				ProcessBuilderFactory processRunner = new ProcessBuilderFactory();
-				try
-				{
-					arguments.add("whereis"); //$NON-NLS-1$
-					arguments.add("wokwi-server"); //$NON-NLS-1$
+		String elfFilePath = IDFUtil.getELFFilePath(project).toOSString();
 
-					Map<String, String> environment = new HashMap<>(System.getenv());
-					IStatus status = processRunner.runInBackground(arguments, org.eclipse.core.runtime.Path.ROOT,
-							environment);
-					if (status == null)
-					{
-						Logger.log(IDFCorePlugin.getPlugin(), IDFCorePlugin.errorStatus("Status can't be null", null)); //$NON-NLS-1$
-					}
-					String wokwiLocation = status.getMessage().split(" ").length > 1 ? status.getMessage().split(" ")[1]
-							: "";
-					wokwiExecutablePath = wokwiLocation.strip();
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-			if (wokwiExecutablePath != null)
-			{
-//				wokwiExecutablePath = wokwiPath.toOSString();
+		String wokwiExecutablePath = getWokwiServerPath();
+		if (StringUtil.isEmpty(wokwiExecutablePath))
+		{
+			Logger.log("wokwi-server executable neither not found nor installed. Please check.");
+			return null;
+		}
 
-				List<String> arguments = new ArrayList<String>();
-				arguments.add(wokwiExecutablePath);
-				arguments.add("--chip");
-				arguments.add(target);
-				arguments.add("--id");
-				arguments.add("345932416223806035");
-				arguments.add(elfFilePath);
+		String diagram_id = null;
+		IFile wokwiConfig = project.getFile(WOKWI_CONFIG_JSON);
+		if (wokwiConfig.exists())
+		{
+			diagram_id = getDiagramId(wokwiConfig);
+		}
 
-				ProcessBuilder processBuilder = new ProcessBuilder(arguments);
-				try
-				{
-					Process process = processBuilder.start();
+		List<String> arguments = new ArrayList<String>();
+		arguments.add(wokwiExecutablePath);
+		arguments.add("--chip"); //$NON-NLS-1$
+		arguments.add(target);
 
-					Thread includePathReaderThread = new Thread("Error Reader")
-					{
-						@Override
-						public void run()
-						{
-							try (BufferedReader reader = new BufferedReader(
-									new InputStreamReader(process.getErrorStream())))
-							{
-								for (String line = reader.readLine(); line != null; line = reader.readLine())
-								{
-									System.out.println("es::" + line);
-								}
-							}
-							catch (IOException e)
-							{
-								CCorePlugin.log(e);
-							}
-						}
-					};
-					includePathReaderThread.start();
+		if (diagram_id != null)
+		{
+			arguments.add("--id"); //$NON-NLS-1$
+			arguments.add(diagram_id);
+		}
+		arguments.add(elfFilePath);
 
-					Thread macroReaderThread = new Thread("Input reader")
-					{
-						@Override
-						public void run()
-						{
-							try (BufferedReader reader = new BufferedReader(
-									new InputStreamReader(process.getInputStream())))
-							{
-								for (String line = reader.readLine(); line != null; line = reader.readLine())
-								{
-									System.out.println("is::" + line);
-								}
-							}
-							catch (IOException e)
-							{
-								CCorePlugin.log(e);
-							}
-						}
-					};
-					macroReaderThread.start();
-
-				}
-				catch (IOException e1)
-				{
-					e1.printStackTrace();
-				}
-
-			}
-
+		try
+		{
+			startWokwiServer(arguments);
+		}
+		catch (IOException e)
+		{
+			Logger.log(e);
 		}
 
 		return null;
+	}
+
+	private void startWokwiServer(List<String> arguments) throws IOException
+	{
+		ProcessBuilder processBuilder = new ProcessBuilder(arguments);
+		Process process = processBuilder.start();
+
+		Thread errReader = new Thread("Error Reader")
+		{
+			@Override
+			public void run()
+			{
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream())))
+				{
+					for (String line = reader.readLine(); line != null; line = reader.readLine())
+					{
+						System.out.println("es::" + line);
+					}
+				}
+				catch (IOException e)
+				{
+					CCorePlugin.log(e);
+				}
+			}
+		};
+		errReader.start();
+
+		Thread inputReader = new Thread("Input reader")
+		{
+			@Override
+			public void run()
+			{
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream())))
+				{
+					for (String line = reader.readLine(); line != null; line = reader.readLine())
+					{
+						System.out.println("is::" + line);
+					}
+				}
+				catch (IOException e)
+				{
+					CCorePlugin.log(e);
+				}
+			}
+		};
+		inputReader.start();
+
+	}
+
+	private String getWokwiServerPath()
+	{
+		// Look for wokwi path
+		IPath wokwiPath = ExecutableFinder.find("wokwi-server", false); //$NON-NLS-1$
+		Logger.log("wokwiPath path:" + wokwiPath); //$NON-NLS-1$
+		if (wokwiPath == null)
+		{
+			List<String> arguments = new ArrayList<String>();
+			ProcessBuilderFactory processRunner = new ProcessBuilderFactory();
+			try
+			{
+				arguments.add("whereis"); //$NON-NLS-1$
+				arguments.add("wokwi-server"); //$NON-NLS-1$
+
+				Map<String, String> environment = new HashMap<>(System.getenv());
+				IStatus status = processRunner.runInBackground(arguments, org.eclipse.core.runtime.Path.ROOT,
+						environment);
+				if (status == null)
+				{
+					Logger.log(IDFCorePlugin.getPlugin(), IDFCorePlugin.errorStatus("Status can't be null", null)); //$NON-NLS-1$
+					return null;
+				}
+				Logger.log(status.getMessage());
+				String wokwiLocation = status.getMessage().split(" ").length > 1 ? status.getMessage().split(" ")[1]
+						: "";
+				return wokwiLocation.strip();
+			}
+			catch (Exception e)
+			{
+				Logger.log(e);
+			}
+		}
+		return null;
+	}
+
+	private String getDiagramId(IFile wokwiConfig)
+	{
+		try
+		{
+			Gson gson = new GsonBuilder().create();
+			JsonReader jsonReader = new JsonReader(new FileReader(wokwiConfig.getFullPath().toFile()));
+			JsonObject jsonObject = gson.fromJson(jsonReader, JsonObject.class);
+			return jsonObject.get("id").toString(); //$NON-NLS-1$
+		}
+		catch (Exception e)
+		{
+			Logger.log(e);
+		}
+		return null;
+	}
+
+	private IProject getProject()
+	{
+		return EclipseUtil.getSelectedProjectInExplorer();
 	}
 
 }
