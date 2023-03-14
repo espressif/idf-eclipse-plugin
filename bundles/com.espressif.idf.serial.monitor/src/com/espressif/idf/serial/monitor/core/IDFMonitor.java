a44503fc3ced0299bef3126976a2f2ec2ffa8d02
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2023-03-15 04:46:47 +0530

    IEP-898:Opening a serial monitor during debug session makes the idf_monitor to reset the board (#715)
    * fix:Opening a serial monitor during debug session makes the idf_monitor
    reset the board
    
    * --no-reset flag supported only from IDF 5.0 and higher

--- bundles/com.espressif.idf.serial.monitor/src/com/espressif/idf/serial/monitor/core/IDFMonitor.java
+++ bundles/com.espressif.idf.serial.monitor/src/com/espressif/idf/serial/monitor/core/IDFMonitor.java
@@ -5,12 +5,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.embedcdt.debug.gdbjtag.core.DebugUtils;
 
 import com.espressif.idf.core.IDFConstants;
 import com.espressif.idf.core.IDFCorePlugin;
@@ -48,10 +55,14 @@ public class IDFMonitor
 		List<String> args = new ArrayList<>();
 		args.add(pythonBinPath);
 		args.add(IDFUtil.getIDFMonitorPythonScriptFile().getAbsolutePath());
-		if(!StringUtil.isEmpty(filterOptions))
+		if (!StringUtil.isEmpty(filterOptions))
 		{
 			args.add("--print_filter"); //$NON-NLS-1$
 			args.add(filterOptions);
+		}
+		if (isDebuggerRunning(project) && isNoResetFlagSupported())
+		{
+			args.add("--no-reset"); //$NON-NLS-1$
 		}
 		args.add("-p"); //$NON-NLS-1$
 		args.add(port);
@@ -83,6 +94,48 @@ public class IDFMonitor
 		return null;
 	}
 
+	private boolean isDebuggerRunning(IProject project)
+	{
+		ILaunchManager launchManager = getLaunchManager();
+		ILaunchConfiguration[] configs;
+		boolean isDebugLaunchRunning = false;
+		try
+		{
+			configs = launchManager.getLaunchConfigurations();
+			for (ILaunchConfiguration config : configs)
+			{
+				IResource[] resources = config.getMappedResources();
+				if (resources != null && resources[0].getProject().equals(project))
+				{
+					isDebugLaunchRunning = DebugUtils.isLaunchConfigurationStarted(config);
+					if (isDebugLaunchRunning) // any debug launchconfig related to the project
+					{
+						return true;
+					}
+				}
+			}
+
+		}
+		catch (CoreException e)
+		{
+			Logger.log(e);
+		}
+		return false;
+	}
+
+	private boolean isNoResetFlagSupported()
+	{
+		String version = IDFUtil.getEspIdfVersion();
+		Pattern p = Pattern.compile("([0-9][.][0-9])"); //$NON-NLS-1$
+		Matcher m = p.matcher(version);
+		return m.find() && Double.parseDouble(m.group(0)) >= 5.0;
+	}
+
+	protected ILaunchManager getLaunchManager()
+	{
+		return DebugPlugin.getDefault().getLaunchManager();
+	}
+
 	private String getMonitorBaudRate()
 	{
 		return new SDKConfigJsonReader(project).getValue("ESPTOOLPY_MONITOR_BAUD"); //$NON-NLS-1$
@@ -90,12 +143,12 @@ public class IDFMonitor
 
 	public Process start() throws Exception
 	{
-		if(!dependenciesAreInstalled())
+		if (!dependenciesAreInstalled())
 		{
 			throw new Exception("The WebSocket dependency is missing and cannot be installed automatically"); //$NON-NLS-1$
 		}
 		List<String> arguments = commandArgsWithSocketServer();
-		
+
 		// command to execute
 		Logger.log(arguments.toString());
 
@@ -133,15 +186,14 @@ public class IDFMonitor
 		LocalTerminal localTerminal = new LocalTerminal(arguments, workingDir.toFile(), environment);
 		return localTerminal.connect();
 	}
-	
+
 	public boolean dependenciesAreInstalled()
 	{
 		InstallToolsHandler installToolsHandler = new InstallToolsHandler();
 		IStatus status = installToolsHandler.handleWebSocketClientInstall();
 		if (status == null || status.getSeverity() == IStatus.ERROR)
 		{
-			Logger.log(IDFCorePlugin.getPlugin(),
-					IDFCorePlugin.errorStatus("Unable to get the process status.", null)); //$NON-NLS-1$
+			Logger.log(IDFCorePlugin.getPlugin(), IDFCorePlugin.errorStatus("Unable to get the process status.", null)); //$NON-NLS-1$
 			return false;
 		}
 
