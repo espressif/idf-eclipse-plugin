773e8d3714ff234731a50522360a40bb3d6df0a4
alirana01 <ali.azamrana@espressif.com>
2023-03-23 13:14:59 +0100

    IEP-911: Changing the shell variable to default display shell as the active display and window can be different

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/DirectorySelectionDialog.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/DirectorySelectionDialog.java
@@ -23,6 +23,7 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -105,7 +106,7 @@ public class DirectorySelectionDialog extends TitleAreaDialog
 			@Override
 			public void widgetSelected(SelectionEvent event)
 			{
-				DirectoryDialog dlg = new DirectoryDialog(shell);
+				DirectoryDialog dlg = new DirectoryDialog(Display.getDefault().getActiveShell());
 				dlg.setFilterPath(text.getText());
 				dlg.setText(Messages.DirectorySelectionDialog_IDFDirLabel);
 				dlg.setMessage(Messages.DirectorySelectionDialog_SelectIDFDirMessage);
