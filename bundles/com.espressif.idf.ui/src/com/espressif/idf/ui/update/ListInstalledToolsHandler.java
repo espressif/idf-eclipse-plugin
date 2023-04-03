764e61381401acd1e3de127a50c243868914deb9
alirana01 <ali.azamrana@espressif.com>
2023-04-03 12:20:39 +0200

    IEP-878: Fixing the git warning
    I have fixed the git warning issue and possibly this will also fix the git spawn error. Further also reverted to a deprecated method for support with older cdt for PersistentPreferences class

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/ListInstalledToolsHandler.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/update/ListInstalledToolsHandler.java
@@ -13,6 +13,7 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 
 import com.espressif.idf.core.IDFConstants;
+import com.espressif.idf.core.IDFEnvironmentVariables;
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
 import com.espressif.idf.core.util.StringUtil;
@@ -77,7 +78,11 @@ public class ListInstalledToolsHandler extends AbstractToolsHandler
 	{
 		List<String> arguments = new ArrayList<String>();
 		arguments.add(IDFConstants.TOOLS_LIST_CMD);
-
+		if (StringUtil.isEmpty(gitExecutablePath))
+		{
+			gitExecutablePath = new IDFEnvironmentVariables().getEnvValue(IDFEnvironmentVariables.GIT_PATH);
+		}
+		
 		runCommand(arguments, console);
 	}
 
