141216874a189919ef3c90629da640da0c3d46f8
alirana01 <ali.azamrana@espressif.com>
2023-04-04 14:35:28 +0200

    IEP-908: Adding the code to support the default workspace directory
    The issue with installer where it sometimes references to the idf toolchain directory for workspace should be resolved now the default workspace is now either in the user home directory or is pointed by the defaultWorkspace property in the esp_idf.json file

---
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/util/EspIdfJsonParser.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Copyright 2023 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.core.util;
+
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.text.MessageFormat;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.espressif.idf.core.logging.Logger;
+
+/**
+ * esp_idf.json parser utility class to parse the 
+ * stored configs for startup in esp_idf.json
+ * @author Ali Azam Rana
+ *
+ */
+public class EspIdfJsonParser
+{
+	/**
+	 * esp-idf.json is file created by the installer
+	 */
+	private static final String ESP_IDF_JSON_FILE = "esp_idf.json"; //$NON-NLS-1$
+	
+	// Variables defined in the esp-idf.json file
+	private static final String GIT_PATH = "gitPath"; //$NON-NLS-1$
+	private static final String IDF_VERSIONS_ID = "idfSelectedId"; //$NON-NLS-1$
+	private static final String IDF_INSTALLED_LIST_KEY = "idfInstalled"; //$NON-NLS-1$
+	private static final String PYTHON_PATH = "python"; //$NON-NLS-1$
+	private static final String IDF_PATH = "path"; //$NON-NLS-1$
+	private static final String DEFAULT_WORSPACE_PATH = "defaultWorkspace"; //$NON-NLS-1$
+
+	private String gitExecutablePath;
+	private String idfVersionId;
+	private String idfPath;
+	private String pythonExecutablePath;
+	private String defaultWorkspaceLocation;
+	private boolean isIdfJsonPresent;
+
+	public void parseJsonAndLoadValues()
+	{
+		// Get the location of the eclipse root directory
+		Location installLocation = Platform.getInstallLocation();
+		URL url = installLocation.getURL();
+		Logger.log("Eclipse Install location::" + url); //$NON-NLS-1$
+		File idf_json_file = new File(url.getPath() + File.separator + ESP_IDF_JSON_FILE);
+		if (!idf_json_file.exists())
+		{
+			Logger.log(MessageFormat.format("esp-idf.json file doesn't exist at this location: '{0}'", url.getPath())); //$NON-NLS-1$
+			isIdfJsonPresent = false;
+			return;
+		}
+		
+		isIdfJsonPresent = true;
+		
+		
+		JSONParser parser = new JSONParser();
+		try
+		{
+			JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(idf_json_file));
+			gitExecutablePath = (String) jsonObj.get(GIT_PATH);
+			idfVersionId = (String) jsonObj.get(IDF_VERSIONS_ID);
+			defaultWorkspaceLocation = (String) jsonObj.get(DEFAULT_WORSPACE_PATH);
+			JSONObject list = (JSONObject) jsonObj.get(IDF_INSTALLED_LIST_KEY);
+			if (list != null)
+			{
+				// selected esp-idf version information
+				JSONObject selectedIDFInfo = (JSONObject) list.get(idfVersionId);
+				idfPath = (String) selectedIDFInfo.get(IDF_PATH);
+				pythonExecutablePath = (String) selectedIDFInfo.get(PYTHON_PATH);
+			}
+		}
+		catch (Exception e) 
+		{
+			Logger.log(e);
+		}
+	}
+
+	public String getGitExecutablePath()
+	{
+		return gitExecutablePath;
+	}
+
+	public String getIdfVersionId()
+	{
+		return idfVersionId;
+	}
+
+	public String getIdfPath()
+	{
+		return idfPath;
+	}
+
+	public String getPythonExecutablePath()
+	{
+		return pythonExecutablePath;
+	}
+
+	public boolean isIdfJsonPresent()
+	{
+		return isIdfJsonPresent;
+	}
+
+	public String getDefaultWorkspaceLocation()
+	{
+		return defaultWorkspaceLocation;
+	}
+}
--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/InitializeToolsStartup.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/InitializeToolsStartup.java
@@ -6,26 +6,17 @@ package com.espressif.idf.ui;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
 import java.text.MessageFormat;
 
 import org.eclipse.cdt.cmake.core.internal.Activator;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.launchbar.core.ILaunchBarManager;
-import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IStartup;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
@@ -34,6 +25,7 @@ import com.espressif.idf.core.build.Messages;
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.resources.OpenDialogListenerSupport;
 import com.espressif.idf.core.resources.ResourceChangeListener;
+import com.espressif.idf.core.util.EspIdfJsonParser;
 import com.espressif.idf.core.util.StringUtil;
 import com.espressif.idf.core.util.ToolChainUtil;
 import com.espressif.idf.ui.dialogs.MessageLinkDialog;
@@ -43,22 +35,11 @@ import com.espressif.idf.ui.update.InstallToolsHandler;
 @SuppressWarnings("restriction")
 public class InitializeToolsStartup implements IStartup
 {
-
-	/**
-	 * esp-idf.json is file created by the installer
-	 */
-	private static final String ESP_IDF_JSON_FILE = "esp_idf.json"; //$NON-NLS-1$
-
-	// Variables defined in the esp-idf.json file
-	private static final String GIT_PATH = "gitPath"; //$NON-NLS-1$
-	private static final String IDF_VERSIONS_ID = "idfSelectedId"; //$NON-NLS-1$
-	private static final String IDF_INSTALLED_LIST_KEY = "idfInstalled"; //$NON-NLS-1$
-	private static final String PYTHON_PATH = "python"; //$NON-NLS-1$
-	private static final String IDF_PATH = "path"; //$NON-NLS-1$
 	private static final String IS_INSTALLER_CONFIG_SET = "isInstallerConfigSet"; //$NON-NLS-1$
 	private static final String DOC_URL = "\"https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/partition-tables.html?highlight=partitions%20csv#creating-custom-tables\""; //$NON-NLS-1$
 
 	private String newIdfPath;
+	private EspIdfJsonParser espIdfJsonParser;
 
 	@Override
 	public void earlyStartup()
@@ -87,19 +68,15 @@ public class InitializeToolsStartup implements IStartup
 		ILaunchBarManager launchBarManager = Activator.getService(ILaunchBarManager.class);
 		launchBarManager.addListener(new LaunchBarListener());
 
-		// Get the location of the eclipse root directory
-		Location installLocation = Platform.getInstallLocation();
-		URL url = installLocation.getURL();
-		Logger.log("Eclipse Install location::" + url);
-		File idf_json_file = new File(url.getPath() + File.separator + ESP_IDF_JSON_FILE);
-		if (!idf_json_file.exists())
+		espIdfJsonParser = new EspIdfJsonParser();
+		espIdfJsonParser.parseJsonAndLoadValues();
+		if (!espIdfJsonParser.isIdfJsonPresent())
 		{
-			Logger.log(MessageFormat.format("esp-idf.json file doesn't exist at this location: '{0}'", url.getPath()));
 			return;
 		}
 		else if (isInstallerConfigSet())
 		{
-			checkForUpdatedVersion(idf_json_file);
+			checkForUpdatedVersion();
 			if (isInstallerConfigSet())
 			{
 				Logger.log("Ignoring esp_idf.json settings as it was configured earilier and idf_path is similar.");
@@ -132,105 +109,51 @@ public class InitializeToolsStartup implements IStartup
 			});
 		}
 
-		// read esp-idf.json file
-		JSONParser parser = new JSONParser();
-		try
+		// read esp-idf.json file configs
+		String gitExecutablePath = espIdfJsonParser.getGitExecutablePath();
+		String idfPath = espIdfJsonParser.getIdfPath();
+		String pythonExecutablePath = espIdfJsonParser.getPythonExecutablePath();
+		IDFEnvironmentVariables idfEnvMgr = new IDFEnvironmentVariables();
+		idfEnvMgr.addEnvVariable(IDFEnvironmentVariables.IDF_PATH, idfPath);
+
+		if (!StringUtil.isEmpty(pythonExecutablePath) && !StringUtil.isEmpty(gitExecutablePath))
 		{
-			JSONObject jsonObj = (JSONObject) parser.parse(new FileReader(idf_json_file));
-			String gitExecutablePath = (String) jsonObj.get(GIT_PATH);
-			String idfVersionId = (String) jsonObj.get(IDF_VERSIONS_ID);
-			JSONObject list = (JSONObject) jsonObj.get(IDF_INSTALLED_LIST_KEY);
-			if (list != null)
-			{
-				// selected esp-idf version information
-				JSONObject selectedIDFInfo = (JSONObject) list.get(idfVersionId);
-				String idfPath = (String) selectedIDFInfo.get(IDF_PATH);
-				String pythonExecutablePath = (String) selectedIDFInfo.get(PYTHON_PATH);
+			ExportIDFTools exportIDFTools = new ExportIDFTools();
+			exportIDFTools.runToolsExport(pythonExecutablePath, gitExecutablePath, null, null);
 
-				// Add IDF_PATH to the eclipse CDT build environment variables
-				IDFEnvironmentVariables idfEnvMgr = new IDFEnvironmentVariables();
-				idfEnvMgr.addEnvVariable(IDFEnvironmentVariables.IDF_PATH, idfPath);
+			// Configure toolchains
+			ToolChainUtil.configureToolChain();
 
-				if (!StringUtil.isEmpty(pythonExecutablePath) && !StringUtil.isEmpty(gitExecutablePath))
-				{
-					ExportIDFTools exportIDFTools = new ExportIDFTools();
-					exportIDFTools.runToolsExport(pythonExecutablePath, gitExecutablePath, null, null);
-
-					// Configure toolchains
-					ToolChainUtil.configureToolChain();
-
-					Preferences scopedPreferenceStore = InstanceScope.INSTANCE.getNode(UIPlugin.PLUGIN_ID);
-					scopedPreferenceStore.putBoolean(InstallToolsHandler.INSTALL_TOOLS_FLAG, true);
-					try
-					{
-						scopedPreferenceStore.flush();
-					}
-					catch (BackingStoreException e)
-					{
-						Logger.log(e);
-					}
-				}
-			}
-
-			// save state
-			Preferences prefs = getPreferences();
-			prefs.putBoolean(IS_INSTALLER_CONFIG_SET, true);
+			Preferences scopedPreferenceStore = InstanceScope.INSTANCE.getNode(UIPlugin.PLUGIN_ID);
+			scopedPreferenceStore.putBoolean(InstallToolsHandler.INSTALL_TOOLS_FLAG, true);
 			try
 			{
-				prefs.flush();
+				scopedPreferenceStore.flush();
 			}
 			catch (BackingStoreException e)
 			{
 				Logger.log(e);
 			}
-
-		}
-		catch (
-				IOException
-				| ParseException e)
-		{
-			Logger.log(e);
 		}
 	}
 
-	private void checkForUpdatedVersion(File idf_json_file)
+	private void checkForUpdatedVersion()
 	{
-		// read esp-idf.json file
-		JSONParser parser = new JSONParser();
-		try (FileReader reader = new FileReader(idf_json_file))
+		String idfPath = espIdfJsonParser.getIdfPath();
+		IDFEnvironmentVariables idfEnvMgr = new IDFEnvironmentVariables();
+		if (idfEnvMgr.getEnvValue(IDFEnvironmentVariables.IDF_PATH).equals(idfPath))
+			return;
+		newIdfPath = idfPath;
+		Preferences prefs = getPreferences();
+		prefs.putBoolean(IS_INSTALLER_CONFIG_SET, false);
+		try
 		{
-			JSONObject jsonObj = (JSONObject) parser.parse(reader);
-			String idfVersionId = (String) jsonObj.get(IDF_VERSIONS_ID);
-			JSONObject list = (JSONObject) jsonObj.get(IDF_INSTALLED_LIST_KEY);
-			if (list == null)
-			{
-				return;
-			}
-			// selected esp-idf version information
-			JSONObject selectedIDFInfo = (JSONObject) list.get(idfVersionId);
-			String idfPath = (String) selectedIDFInfo.get(IDF_PATH);
-			IDFEnvironmentVariables idfEnvMgr = new IDFEnvironmentVariables();
-			if (idfEnvMgr.getEnvValue(IDFEnvironmentVariables.IDF_PATH).equals(idfPath))
-				return;
-			newIdfPath = idfPath;
-			Preferences prefs = getPreferences();
-			prefs.putBoolean(IS_INSTALLER_CONFIG_SET, false);
-			try
-			{
-				prefs.flush();
-			}
-			catch (BackingStoreException e)
-			{
-				Logger.log(e);
-			}
+			prefs.flush();
 		}
-		catch (
-				IOException
-				| ParseException e)
+		catch (BackingStoreException e)
 		{
 			Logger.log(e);
 		}
-
 	}
 
 	private Preferences getPreferences()
