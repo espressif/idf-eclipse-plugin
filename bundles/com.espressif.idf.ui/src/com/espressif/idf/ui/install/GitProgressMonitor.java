3b3ecc8fe365aaf4b254900f82208c3f3dbae38f
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2023-05-15 12:05:42 +0530

    Fix: Compatible fixes with Eclipse 2022-03 JGIT plugin (#763)
    * Fix: Compatible fixes with Eclipse 2022-03 JGIT plugin
    
    * Fix: Add backward compatibility of jgit plugin

--- bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/install/GitProgressMonitor.java
+++ bundles/com.espressif.idf.ui/src/com/espressif/idf/ui/install/GitProgressMonitor.java
@@ -5,6 +5,7 @@
 package com.espressif.idf.ui.install;
 
 import java.text.MessageFormat;
+import java.time.Duration;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jgit.lib.BatchingProgressMonitor;
@@ -28,34 +29,59 @@ public class GitProgressMonitor extends BatchingProgressMonitor
 		this.monitor = monitor;
 	}
 
+	protected void onUpdate(final String taskName, final int workCurr)
+	{
+		onUpdate(taskName, workCurr, null);
+	}
+
+	protected void onUpdate(final String taskName, final int workCurr, Duration duration)
+	{
+		console.println(MessageFormat.format("{0}  {1} ", taskName, workCurr)); //$NON-NLS-1$
+	}
+
+	protected void onUpdate(final String taskName, final int workCurr, final int workTotal, final int percentDone)
+	{
+		onUpdate(taskName, workCurr, workTotal, percentDone, null);
+	}
+
+	protected void onUpdate(final String taskName, final int workCurr, final int workTotal, final int percentDone,
+			Duration duration)
+	{
+		console.println(
+				MessageFormat.format("{0} {1}, total {2} {3}% Completed", taskName, workCurr, workTotal, percentDone)); //$NON-NLS-1$
+	}
+
+	protected void onEndTask(final String taskName, final int workCurr)
+	{
+		onEndTask(taskName, workCurr, null);
+	}
+
+	protected void onEndTask(final String taskName, final int workCurr, Duration duration)
+	{
+		console.println(MessageFormat.format("Finished {0}  {1}", taskName, workCurr)); //$NON-NLS-1$
+	}
+
+	protected void onEndTask(final String taskName, final int workCurr, final int workTotal, final int percentDone)
+	{
+		onEndTask(taskName, workCurr, workTotal, percentDone, null);
+	}
+
+	protected void onEndTask(final String taskName, final int workCurr, final int workTotal, final int percentDone,
+			Duration duration)
+	{
+		console.println(MessageFormat.format("Finished {0} {1}, total {2} {3}% Completed", taskName, workCurr, //$NON-NLS-1$
+				workTotal, percentDone));
+	}
+
 	@Override
-    protected void onUpdate(final String taskName, final int workCurr) {
-		console.println(MessageFormat.format("{0}  {1}", taskName, workCurr)); //$NON-NLS-1$
-    }
-
-    @Override
-    protected void onUpdate(final String taskName, final int workCurr, final int workTotal, final int percentDone) {
-    	console.println(MessageFormat.format("{0} {1}, total {2} {3}% Completed", taskName, workCurr, workTotal, percentDone)); //$NON-NLS-1$
-    }
-
-    @Override
-    protected void onEndTask(final String taskName, final int workCurr) {
-    	console.println(MessageFormat.format("Finished {0}  {1}", taskName, workCurr)); //$NON-NLS-1$
-    }
-
-    @Override
-    protected void onEndTask(final String taskName, final int workCurr, final int workTotal, final int percentDone) {
-    	console.println(MessageFormat.format("Finished {0} {1}, total {2} {3}% Completed", taskName, workCurr, workTotal, percentDone)); //$NON-NLS-1$
-    }
-    
-    @Override
-    public boolean isCancelled()
-    {
-    	if (monitor.isCanceled())
-    	{
-    		console.println("Download cancellation initiated by user!"); //$NON-NLS-1$
-    		return true;
-    	}
+	public boolean isCancelled()
+	{
+		if (monitor.isCanceled())
+		{
+			console.println("Download cancellation initiated by user!"); //$NON-NLS-1$
+			return true;
+		}
 		return false;
-    }
+	}
+
 }
