1f3ef2dde9f9976ae17b29d0e63de9aa021fb5b5
Denys Almazov <sigma@f0xz.org.ua>
2023-08-16 11:42:01 +0300

    change default build hints status to true

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/IDFCorePreferenceConstants.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/IDFCorePreferenceConstants.java
@@ -24,7 +24,7 @@ public class IDFCorePreferenceConstants
 	public static final String CMAKE_CCACHE_STATUS = "cmakeCCacheStatus"; //$NON-NLS-1$
 	public static final String AUTOMATE_BUILD_HINTS_STATUS = "automateHintsStatus"; //$NON-NLS-1$
 	public static final boolean CMAKE_CCACHE_DEFAULT_STATUS = true;
-	public static final boolean AUTOMATE_BUILD_HINTS_DEFAULT_STATUS = false;
+	public static final boolean AUTOMATE_BUILD_HINTS_DEFAULT_STATUS = true;
 	/**
 	 * Returns the node in the preference in the given context.
 	 *
