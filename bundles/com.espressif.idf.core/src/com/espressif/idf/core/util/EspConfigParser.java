177aeb5119792dc257e769c554d00dc73908ca95
alirana01 <ali.azamrana@espressif.com>
2023-11-06 12:38:17 +0100

    unwanted import removed

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/util/EspConfigParser.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/util/EspConfigParser.java
@@ -10,7 +10,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
