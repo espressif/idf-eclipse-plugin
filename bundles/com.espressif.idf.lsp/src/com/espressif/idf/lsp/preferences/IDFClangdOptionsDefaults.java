fba6c164048fde974cfa3ce3293299d087282098
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2023-12-28 13:04:09 +0530

    fix: auto-detect clangd from the PATH and set in preferences

--- bundles/com.espressif.idf.lsp/src/com/espressif/idf/lsp/preferences/IDFClangdOptionsDefaults.java
+++ bundles/com.espressif.idf.lsp/src/com/espressif/idf/lsp/preferences/IDFClangdOptionsDefaults.java
@@ -1,8 +1,12 @@
 package com.espressif.idf.lsp.preferences;
 
+import java.util.Optional;
+
 import org.eclipse.cdt.lsp.clangd.BuiltinClangdOptionsDefaults;
 import org.eclipse.cdt.lsp.clangd.ClangdOptionsDefaults;
 import org.osgi.service.component.annotations.Component;
+
+import com.espressif.idf.core.util.IDFUtil;
 
 @SuppressWarnings("restriction")
 @Component(service = ClangdOptionsDefaults.class, property = { "service.ranking:Integer=100" })
@@ -10,12 +14,13 @@ public class IDFClangdOptionsDefaults extends BuiltinClangdOptionsDefaults {
 
 	@Override
 	public String clangdPath() {
-		return super.clangdPath();//TODO: esp-clangd path
+		return Optional.ofNullable(IDFUtil.findCommandFromBuildEnvPath("clangd")) //$NON-NLS-1$
+				.orElse("clangd"); //$NON-NLS-1$
 	}
 
 	@Override
 	public String queryDriver() {
-		return super.queryDriver(); //TODO:esp xtensa-gcc path
+		return super.queryDriver(); // TODO:esp xtensa-gcc path
 	}
 
 }
