00f1666fbce6a0d8e756bc0fc675192c5360e440
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2024-01-24 09:42:52 +0100

    common method moved to util class

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/util/IDFUtil.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/util/IDFUtil.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
