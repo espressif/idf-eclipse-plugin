efd25d30439f75ea53e050356e78cf3e15995e06
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2024-01-24 09:41:29 +0100

    updated IDFEnvVars to make the var names constant

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/IDFEnvironmentVariables.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/IDFEnvironmentVariables.java
@@ -42,11 +42,11 @@ public class IDFEnvironmentVariables
 
 	public static final String ESP_IDF_VERSION = "ESP_IDF_VERSION"; //$NON-NLS-1$
 
-	public static String GIT_PATH ="GIT_PATH"; //$NON-NLS-1$
+	public static final String GIT_PATH ="GIT_PATH"; //$NON-NLS-1$
 	
-	public static String PYTHON_EXE_PATH ="PYTHON_EXE_PATH"; //$NON-NLS-1$
+	public static final String PYTHON_EXE_PATH ="PYTHON_EXE_PATH"; //$NON-NLS-1$
 	
-	public static String IDF_MAINTAINER = "IDF_MAINTAINER"; //$NON-NLS-1$
+	public static final String IDF_MAINTAINER = "IDF_MAINTAINER"; //$NON-NLS-1$
 
 
 	/**
