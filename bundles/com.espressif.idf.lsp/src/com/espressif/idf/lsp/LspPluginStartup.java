afb9f26bf169b9116480d656653f5e8dcafe78d8
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2024-04-08 18:15:01 +0530

    fix: disable cdt indexer with the lsp plugin activation (#930)
    * fix: disable cdt indexer with the lsp plugin activation
    
    * fix: explaining why the CDT indexer is disabled by default

---
+++ bundles/com.espressif.idf.lsp/src/com/espressif/idf/lsp/LspPluginStartup.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright 2024 Espressif Systems (Shanghai) PTE LTD. All rights reserved.
+ * Use is subject to license terms.
+ *******************************************************************************/
+package com.espressif.idf.lsp;
+
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.dom.IPDOMManager;
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author Kondal Kolipaka <kondal.kolipaka@espressif.com>
+ */
+public class LspPluginStartup implements IStartup
+{
+
+	@Override
+	public void earlyStartup()
+	{
+		// Disabling the CDT indexer by default when the LSP plugin so that we could only use C/C++ LSP Server
+		//This will also avoid parser exceptions and unnecessary processing overhead which is not needed.
+		CCorePlugin.getIndexManager().setDefaultIndexerId(IPDOMManager.ID_NO_INDEXER);
+	}
+
+}
