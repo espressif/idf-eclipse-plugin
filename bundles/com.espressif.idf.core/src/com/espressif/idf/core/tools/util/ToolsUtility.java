0d29bfd25ec69904cbf02859904d3f1a23b45761
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2025-01-23 15:45:42 +0100

    IEP-1334: code cleanup (Active Review) (#1125)
    * cleanup of unused classes and code

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/util/ToolsUtility.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/util/ToolsUtility.java
@@ -6,10 +6,7 @@ package com.espressif.idf.core.tools.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,34 +24,6 @@ import com.espressif.idf.core.tools.vo.IdfInstalled;
  */
 public class ToolsUtility
 {
-	/**
-	 * Gets the file checksum based on the provided message digest
-	 * 
-	 * @param digest
-	 * @param file
-	 * @return
-	 * @throws IOException
-	 */
-	public static String getFileChecksum(MessageDigest digest, File file) throws IOException
-	{
-		FileInputStream fis = new FileInputStream(file);
-		byte[] byteArray = new byte[1024];
-		int bytesCount = 0;
-		while ((bytesCount = fis.read(byteArray)) != -1)
-		{
-			digest.update(byteArray, 0, bytesCount);
-		}
-		;
-		fis.close();
-		byte[] bytes = digest.digest();
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < bytes.length; i++)
-		{
-			sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-		}
-		return sb.toString();
-	}
-	
 	public static String getIdfVersion(IdfInstalled idfInstalled, String gitPath)
 	{
 		String activationScript = idfInstalled.getActivationScript();
