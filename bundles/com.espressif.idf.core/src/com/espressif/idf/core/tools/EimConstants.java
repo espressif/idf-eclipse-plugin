249ada329962f751516a2a1fa858758e25411b7f
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2025-02-04 14:10:34 +0100

    fixing POSIX path resolution

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/EimConstants.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/EimConstants.java
@@ -6,7 +6,7 @@ public interface EimConstants
 
 	String EIM_URL = "https://dl.espressif.com/dl/eim/"; //$NON-NLS-1$
 	
-	String EIM_POSIX_PATH = "~/.espressif/tools/eim_idf.json"; //$NON-NLS-1$
+	String EIM_POSIX_PATH = System.getProperty("user.home").concat("/.espressif/tools/eim_idf.json"); //$NON-NLS-1$ //$NON-NLS-2$
 	
 	String INSTALL_TOOLS_FLAG = "INSTALL_TOOLS_FLAG"; //$NON-NLS-1$
 	
