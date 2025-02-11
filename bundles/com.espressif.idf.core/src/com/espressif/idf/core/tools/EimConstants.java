39fd98a7dcd86b3236d20954fa8864f0c448b007
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2025-02-11 15:51:54 +0100

    file name updated

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/EimConstants.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/EimConstants.java
@@ -2,7 +2,7 @@ package com.espressif.idf.core.tools;
 
 public interface EimConstants
 {
-	String EIM_WIN_PATH = "C:\\Espressif\\tools\\esp_ide.json"; //$NON-NLS-1$
+	String EIM_WIN_PATH = "C:\\Espressif\\tools\\eim_idf.json"; //$NON-NLS-1$
 
 	String EIM_URL = "https://dl.espressif.com/dl/eim/"; //$NON-NLS-1$
 	
