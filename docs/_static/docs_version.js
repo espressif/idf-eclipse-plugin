33e5716626b14bc99629c1102fd5371745598a6a
Ali Azam Rana <85216275+alirana01@users.noreply.github.com>
2025-07-02 12:57:48 +0200

    version.js added

---
+++ docs/_static/docs_version.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_VERSIONS = {
+    DEFAULTS: { has_targets: false,
+                supported_targets: [ "esp32" ]
+              },
+    VERSIONS: [
+        { name: "latest" },
+        { name: "release/v4.0.0", pre_release: true }
+    ]
+};
