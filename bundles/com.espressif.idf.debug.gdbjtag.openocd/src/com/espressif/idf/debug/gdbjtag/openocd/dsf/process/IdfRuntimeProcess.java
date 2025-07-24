ae42e7d7024b4583bed04753376d15876d9ab5e1
Kondal Kolipaka <kondal.kolipaka@espressif.com>
2025-07-24 13:32:34 +0530

    fix: write openocd debug output to a file (#1274)
    * fix: write openocd debug output to a file
    
    * docs: udpate docs on the openocd.log log output
    
    * feat: make fileWriter autoclosable and apply formatting
    
    * fix: fixed the behavior when append is not used
    
    * fix: removed useless code
    
    ---------
    
    Co-authored-by: Denys Almazov <almazovdenys@gmail.com>

--- bundles/com.espressif.idf.debug.gdbjtag.openocd/src/com/espressif/idf/debug/gdbjtag/openocd/dsf/process/IdfRuntimeProcess.java
+++ bundles/com.espressif.idf.debug.gdbjtag.openocd/src/com/espressif/idf/debug/gdbjtag/openocd/dsf/process/IdfRuntimeProcess.java
@@ -10,19 +10,20 @@ import java.nio.charset.UnsupportedCharsetException;
 import java.util.Map;
 
 import org.eclipse.cdt.dsf.gdb.launching.GDBProcess;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IStreamsProxy;
-import org.eclipse.debug.core.model.RuntimeProcess;
 import org.eclipse.debug.internal.core.NullStreamsProxy;
+import org.eclipse.debug.ui.IDebugUIConstants;
 
-import com.espressif.idf.core.util.StringUtil;
 import com.espressif.idf.debug.gdbjtag.openocd.dsf.process.monitors.StreamsProxy;
 
 /**
- * Customised process class for the 
- * Idf based processes that will require a 
- * custom console based on the settings provided in the espressif configurations
+ * Customised process class for the Idf based processes that will require a custom console based on the settings
+ * provided in the espressif configurations
+ * 
  * @author Ali Azam Rana
  *
  */
@@ -30,6 +31,7 @@ import com.espressif.idf.debug.gdbjtag.openocd.dsf.process.monitors.StreamsProxy
 public class IdfRuntimeProcess extends GDBProcess
 {
 	private boolean fCaptureOutput = true;
+	private StreamsProxy streamsProxy;
 
 	public IdfRuntimeProcess(ILaunch launch, Process process, String name, Map<String, String> attributes)
 	{
@@ -60,7 +62,39 @@ public class IdfRuntimeProcess extends GDBProcess
 				DebugPlugin.log(e);
 			}
 		}
-		StreamsProxy streamsProxy = new StreamsProxy(this, getSystemProcess(), charset, getLabel());
+		// Use Eclipse Common tab attribute for output file and append
+		final String outputFileName = getAttributeSafe(getLaunch().getLaunchConfiguration()::getAttribute,
+				IDebugUIConstants.ATTR_CAPTURE_IN_FILE, "");
+
+		final boolean append = getAttributeSafe(getLaunch().getLaunchConfiguration()::getAttribute,
+				IDebugUIConstants.ATTR_APPEND_TO_FILE, false);
+		streamsProxy = new StreamsProxy(this, getSystemProcess(), charset, getLabel(), outputFileName, append);
 		return streamsProxy;
+	}
+
+	@Override
+	public void terminate() throws DebugException
+	{
+		super.terminate();
+		streamsProxy.kill();
+	}
+
+	private <T> T getAttributeSafe(AttributeGetter<T> getter, String attribute, T defaultValue)
+	{
+		try
+		{
+			return getter.get(attribute, defaultValue);
+		}
+		catch (CoreException e)
+		{
+			DebugPlugin.log(e);
+			return defaultValue;
+		}
+	}
+
+	@FunctionalInterface
+	interface AttributeGetter<T>
+	{
+		T get(String attribute, T defaultValue) throws CoreException;
 	}
 }
