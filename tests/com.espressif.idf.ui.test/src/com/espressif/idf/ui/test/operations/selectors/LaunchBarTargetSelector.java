c26e413664c8db53dd9574877cbc589f7b60abd2
Andrii Filippov <andrii.filippov@espressif.com>
2025-09-11 12:23:34 +0200

    ci: add 'Delete target' test

--- tests/com.espressif.idf.ui.test/src/com/espressif/idf/ui/test/operations/selectors/LaunchBarTargetSelector.java
+++ tests/com.espressif.idf.ui.test/src/com/espressif/idf/ui/test/operations/selectors/LaunchBarTargetSelector.java
@@ -8,6 +8,8 @@ package com.espressif.idf.ui.test.operations.selectors;
 import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.widgetOfType;
 import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.withText;
 
+import java.util.List;
+
 import org.eclipse.launchbar.ui.controls.internal.CSelector;
 import org.eclipse.launchbar.ui.controls.internal.LaunchBarWidgetIds;
 import org.eclipse.launchbar.ui.controls.internal.TargetSelector;
@@ -15,7 +17,9 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.swt.widgets.Widget;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.SWTBotWidget;
@@ -23,6 +27,7 @@ import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
 import org.eclipse.swtbot.swt.finder.results.Result;
 import org.eclipse.swtbot.swt.finder.widgets.AbstractSWTBotControl;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotLabel;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 
 /**
@@ -120,5 +125,51 @@ public class LaunchBarTargetSelector extends AbstractSWTBotControl<CSelector>
 		Point itemToSelectLocation = syncExec((Result<Point>) itemToSelect::getLocation);
 		clickOnInternalWidget(itemToSelectLocation.x, itemToSelectLocation.y, itemToSelect);
 		return this;
+	}
+
+	public boolean isTargetPresent(String text)
+	{
+		click();
+
+		try
+		{
+			SWTBotShell swtBotShell = bot().shellWithId(LaunchBarWidgetIds.POPUP);
+			ScrolledComposite scrolledComposite = swtBotShell.bot().widget(widgetOfType(ScrolledComposite.class));
+
+			int numberOfItemsInScrolledComp = syncExec(() ->
+				((Composite) scrolledComposite.getChildren()[0]).getChildren().length
+			);
+
+			if (numberOfItemsInScrolledComp > NUM_FOR_FILTER_POPUP)
+			{
+				swtBotShell.bot().text().setText(text);
+
+				List<? extends Widget> labels = swtBotShell.bot().widgets(widgetOfType(Label.class));
+				for (Widget widget : labels)
+				{
+					String labelText = syncExec(() -> ((Label) widget).getText());
+					if (labelText.equals(text))
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+			else
+			{
+				Widget itemToCheck = swtBotShell.bot().widget(withText(text));
+				String labelText = syncExec(() -> ((Label) itemToCheck).getText());
+				return labelText.equals(text);
+			}
+		}
+		catch (WidgetNotFoundException e)
+		{
+			return false;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
