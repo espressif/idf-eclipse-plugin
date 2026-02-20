4b78858f43c884f5cc984e601a31e09fc36e0f78
Denys Almazov <almazovdenys@gmail.com>
2026-02-20 21:15:08 +0200

    fix: removing copyOpenOcdRules since eim is handling it now

--- bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/Messages.java
+++ bundles/com.espressif.idf.core/src/com/espressif/idf/core/tools/Messages.java
@@ -7,13 +7,6 @@ public class Messages extends NLS
 {
 	private static final String BUNDLE_NAME = "com.espressif.idf.core.tools.messages"; //$NON-NLS-1$
 	
-	public static String InstallToolsHandler_CopyingOpenOCDRules;
-	public static String InstallToolsHandler_OpenOCDRulesCopyWarning;
-	public static String InstallToolsHandler_OpenOCDRulesCopyWarningMessage;
-	public static String InstallToolsHandler_OpenOCDRulesNotCopied;
-	public static String InstallToolsHandler_OpenOCDRulesCopyPaths;
-	public static String InstallToolsHandler_OpenOCDRulesCopyError;
-	public static String InstallToolsHandler_OpenOCDRulesCopied;
 	
 	public static String EimVersionMismatchExceptionMessage;
 	public static String EimVersionMismatchExceptionMessageTitle;
