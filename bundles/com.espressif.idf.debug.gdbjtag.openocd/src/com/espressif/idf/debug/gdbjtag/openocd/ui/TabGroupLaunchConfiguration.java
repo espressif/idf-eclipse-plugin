0567ece41b0f336fff3f208236c38c4a960eb3fb
Denys Almazov <almazovdenys@gmail.com>
2026-03-04 16:44:40 +0200

    feat: moving hardcoded tab declaration to the plugin.xml

--- bundles/com.espressif.idf.debug.gdbjtag.openocd/src/com/espressif/idf/debug/gdbjtag/openocd/ui/TabGroupLaunchConfiguration.java
+++ bundles/com.espressif.idf.debug.gdbjtag.openocd/src/com/espressif/idf/debug/gdbjtag/openocd/ui/TabGroupLaunchConfiguration.java
@@ -18,10 +18,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
-import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
-import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 
 import com.espressif.idf.core.logging.Logger;
 import com.espressif.idf.core.util.IDFUtil;
@@ -33,24 +30,7 @@ public class TabGroupLaunchConfiguration extends AbstractLaunchConfigurationTabG
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode)
 	{
-
-		// Normally the tabs should be defined in the plugin.xml, and
-		// here just return an empty array:
-		// setTabs(new ILaunchConfigurationTab[0]);
-		// But the first attempt to make this work failed, it seems
-		// there is something missing in the definitions and
-		// the tab extensions are filtered out.
-
-		// To avoid these problems and for a better control,
-		// we manually define the tabs here.
-
-		TabStartup tabStartup = new TabStartup();
-
-		ILaunchConfigurationTab tabs[] = new ILaunchConfigurationTab[] { new TabMain(), new TabDebugger(tabStartup),
-				tabStartup, new SourceLookupTab(), new CommonTab(), new TabSvdTarget() };
-
-		setTabs(tabs);
-
+		setTabs();
 	}
 
 	@Override
